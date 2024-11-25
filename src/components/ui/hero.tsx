21b6741802559dd47c4ec4c98d5f69eee9e6479b
taufanrizki <taufan.rizki123@gmail.com>
2024-11-25 20:40:59 +0700

    update hero judul

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -23,8 +23,8 @@ export default function Example() {
                     <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                     </div>
                     <div className="text-center">
-                        <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-                            Data to enrich your online business
+                        <h1 className="text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                            Penggalangan Dana Untuk Bantuan Kemanusiaan
                         </h1>
                         <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
                             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
